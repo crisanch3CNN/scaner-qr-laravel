1fd658323c3e3be477a0674e3a82828a3f84a5db
crisanch <cristian.sanchez@triciclos.cl>
2022-07-27 12:50:08 -0400

    feat(scan-qr): se logra personalizar el escaneo de un codigo qr y capturar data

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test2', function () {
+    return view('scan-qr-2');
+});
 
 Route::middleware([
     'auth:sanctum',
